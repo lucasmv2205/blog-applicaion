de214d13dc42942cc58b0aa7ba3030d624c0bedf
lucasmv2205 <lmartins2205@gmail.com>
2022-04-05 21:21:18 -0300

    home page with styles, structure and other configuration of the application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +27,66 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Título</strong>
+              <p>Pensando em sincrnizaçao com equipamento</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Lucas
+                </li>
+              </ul>
+            </a>
+          </Link>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Título</strong>
+              <p>Pensando em sincrnizaçao com equipamento</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Lucas
+                </li>
+              </ul>
+            </a>
+          </Link>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Título</strong>
+              <p>Pensando em sincrnizaçao com equipamento</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Lucas
+                </li>
+              </ul>
+            </a>
+          </Link>
+          <button type="button">Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
